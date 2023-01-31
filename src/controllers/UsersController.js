c36231db70c08b2ea0e73dc2836917f3be19ac5d
FelipeMMoraes <felipe.moraes8@hotmail.com>
2023-01-31 15:45:53 -0300

    feat: UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,8 +1,32 @@
-class UsersController{
-  create(request, response) {
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
+const { hash } = require("bcrypt")
+
+class UsersController {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    response.json({ name, email, password });
+    if (!name || !email || !password.lenght >= 6) {
+      throw new AppError ("Não foi possivel fazer cadastro, verifique as informações inseridas.")
+    }
+
+    const userExist = await knex.select("email")
+    .where({ email })
+    .from("users")
+    .first()
+
+    if(userExist){
+      throw new AppError("Este email já está em uso")
+    }
+
+    const hashedPassword = await hash(password, 8)
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    return response.json();
   }
 
 
